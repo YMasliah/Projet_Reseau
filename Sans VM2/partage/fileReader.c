832f51df5fef2490e424dec1d46a8fd006893e16
Damien Boudoire <damien.boudoire@etu.univ-amu.fr>
2016-12-06 14:11:52 +0100

    fonction getConf finie

--- Sans VM2/partage/fileReader.c
+++ Sans VM2/partage/fileReader.c
@@ -1,17 +1,56 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int getConf(char *filePath, char *tun, char *inip, char *inport, char *options, char *outip, char* outport)
+void readConf(char *filePath, char *tun, char *inip, char *inport, char *options, char *outip, char* outport)
 {
-  int fd;
-  if((fd = fopen(filePath)) < 0)
+  FILE *fd;
+
+  if((fd = fopen(filePath, "r")) < 0)
   {
     perror("File not found");
     exit(2);
   }
 
+  char buff[50];
+  
+  fgets(buff, 50, fd);
+  if((fgets(buff, 50, fd)) && (sscanf(buff, "  tun=%s", tun) < 0))
+  {
+    perror("cannot find tun configuration");
+    exit(3);
+  }
+  
+  if((fgets(buff, 50, fd)) && (fscanf(fd, "  inip=%s", inip) < 0))
+  {
+    perror("cannot find ipin configuration");
+    exit(4);
+  }
 
+  if((fgets(buff, 50, fd)) && (fscanf(fd, "  inport=%s", inport) < 0))
+  {
+    perror("cannot find inport configuration");
+    exit(5);
+  }
 
+  if((fgets(buff, 50, fd)) && (fscanf(fd, "  options=%s", options) < 0))
+  {
+    perror("cannot find options configuration");
+    exit(6);
+  }
 
+  fgets(buff, 50, fd);
+
+  if((fgets(buff, 50, fd)) && (fscanf(fd, "  outip=%s", outip) < 0))
+  {
+    perror("cannot find outin configuration");
+    exit(7);
+  }
+
+  if((fgets(buff, 50, fd)) && (fscanf(fd, "  outport=%s", outport) < 0))
+  {
+    perror("cannot find outport configuration");
+    exit(8);
+  }
 
   close(fd);
 }
