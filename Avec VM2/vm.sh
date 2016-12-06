6e49ac951168281d02daa408ce02e983195aa3b0
Yann Masliah <yann.masliah@etu.univ-amu.fr>
2016-12-06 11:28:59 +0100

    petite mise en forme

---
+++ Avec VM2/vm.sh
@@ -0,0 +1,15 @@
+VBoxManage setproperty machinefolder "/virtualbox/tmp"
+cd VM1 
+vagrant $* &
+cd ../VM2 
+vagrant $* &
+cd ../VM3 
+vagrant $* &
+cd ../VM1-6 
+vagrant $* & 
+cd ../VM2-6  
+vagrant $* & 
+cd ../VM3-6  
+vagrant $* & 
+cd ../
+
